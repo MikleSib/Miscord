4b16e92a07435d6b18b72659f61e9fd3f11fcd9a
MikleSib <Mishatrof21@gmail.com>
2025-06-21 17:09:01 +0700

    f

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
       {
         source: '/api/:path*',
         destination: process.env.NEXT_PUBLIC_API_URL
-          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
-          : 'http://195.19.93.203:8000/api/:path*',
+          ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+          : 'https://miscord.ru/api/:path*',
       },
     ];
   },
