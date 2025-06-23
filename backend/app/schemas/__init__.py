16b49fcead73872c8327fca8fd31168eaeece5fd
MikleSib <Mishatrof21@gmail.com>
2025-06-23 21:51:50 +0700

    Update __init__.py

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,12 +1,12 @@
 from .user import UserCreate, UserResponse, UserUpdate
-from .channel import ChannelCreate, ChannelResponse, ChannelUpdate
+from .channel import ChannelCreate, Channel, ChannelUpdate
 from .message import MessageCreate, MessageUpdate, Message, MessageEvent
 from .attachment import Attachment
 from .reaction import ReactionCreate, ReactionResponse, ReactionToggleRequest
 
 __all__ = [
     "UserCreate", "UserResponse", "UserUpdate",
-    "ChannelCreate", "ChannelResponse", "ChannelUpdate", 
+    "ChannelCreate", "Channel", "ChannelUpdate", 
     "MessageCreate", "MessageUpdate", "Message", "MessageEvent",
     "Attachment",
     "ReactionCreate", "ReactionResponse", "ReactionToggleRequest",
